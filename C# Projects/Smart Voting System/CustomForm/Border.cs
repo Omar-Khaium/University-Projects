a3730eeab48e7450e4805241641020c240cfc1c4
Omar Khaium Chowdhury <40541185+Omar-Khaium@users.noreply.github.com>
2018-07-28 23:34:26 +0600

    Add files via upload

---
+++ C# Projects/Smart Voting System/CustomForm/Border.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CustomForm
+{
+    public partial class Border
+    {
+        
+
+
+        /// <summary>
+        /// Lấy hoặc đặt màu của vền form.
+        /// </summary>
+        public Color BorderColor
+        {
+            get { return this.BackColor; }
+            set
+            {
+                this.BackColor = value;
+                _frm.CreateGraphics().DrawRectangle(new Pen(Title.BackColor), new Rectangle(0, 0, _frm.Width - 1, _frm.Height - 1));
+            }
+        }
+
+        /// <summary>
+        /// Lấy hoặc đặt màu của tên form được hiển thị trên thanh title.
+        /// </summary>
+        public Color TitleColor
+        {
+            get { return Title.ForeColor; }
+            set
+            {
+                Title.ForeColor = value;
+            }
+        }
+        
+
+        /// <summary>
+        /// Tạo một thể hiện mới với các tham số được truyền vào
+        /// </summary>
+        /// <param name="frm">Form chứa Border này</param>
+        /// <param name="backcolor">Màu nền của Border</param>
+        /// <param name="textcolor">Màu chữ tiêu đề của Border</param>
+        /// <param name="textsize">Kích thước chữ tiêu đề của Border</param>
+        /// <param name="backgroundMoveable">Chỉ định khả năng di chuyển form bằng background</param>
+        /// <param name="resizeable">Chỉ định khả năng thay đổi kích thước của form</param>
+        public Border(Form frm, Color backcolor, Color textcolor, bool backgroundMoveable)
+        {
+            _frm = frm;
+            _frm.FormBorderStyle = FormBorderStyle.None;
+            BackgroundMoveable = backgroundMoveable;
+            
+            AddFormMoveableControls(this, Title);
+            
+            InitializeComponent(backcolor, textcolor);
+
+            // add event to control
+            frm.MouseMove += delegate(object sender, MouseEventArgs e)
+            {
+                if (e.Y < Height / 2)
+                {
+                    Timer1.Start();
+                    Timer2.Stop();
+                }
+                else
+                {
+                    Timer2.Start();
+                    Timer1.Stop();
+                }
+            };
+
+            frm.SizeChanged += delegate(object sender, EventArgs e)
+            {
+                this.Width = frm.Width;
+                CloseBox.Location = new Point(Width - CloseBox.Width, 0);
+                NormnalBox.Location = new Point(Width - NormnalBox.Width * 2, 0);
+                MiniBox.Location = new Point(Width - MiniBox.Width * 3, 0);
+                frm.Refresh();
+            };
+
+            frm.Paint += delegate(object sender, PaintEventArgs e)
+            {
+                e.Graphics.DrawRectangle(new Pen(Title.BackColor), new Rectangle(0, 0, frm.Width - 1, frm.Height - 1));
+            };
+
+
+            Controls.Add(Title);
+            Controls.Add(OpacityTrackbar);
+            Controls.Add(MiniBox);
+            Controls.Add(NormnalBox);
+            Controls.Add(CloseBox);
+            frm.Controls.Add(this);
+            this.BringToFront();
+        }
+    }
+}
---
+++ C# Projects/Smart Voting System/CustomForm/BorderResizer.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace CustomForm
+{
+    public partial class Border
+    {   
+    }
+}
